76b7a0f19bd602835e1832762dd1d73f9865fe4d
Isabella Azu <smile@isabella.jp>
2014-02-11 19:36:48 +0900

    Create template_a.php
    テンプレート：埋め込み先のみ用意

---
+++ template_a.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<title>Upload Preview Test</title>
+</head>
+<body>
+
+
+
+<!--
+変数情報
+ディレクトリ	：TEMP_a
+ファイル名	：TEMP_b
+名前			：TEMP_c
+身長			：TEMP_d
+体重			：TEMP_e
+血液型		：TEMP_f
+-->
+
+
+<%TEMP_a>
+<%TEMP_b>
+<%TEMP_c>
+<%TEMP_d>
+<%TEMP_e>
+<%TEMP_f>
+
+
+</body>
+</html>
